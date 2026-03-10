bca80b7785422690cc8d76b48fca0b5ed4287e78
PrometheusAgnostic <dharius19000@gmail.com>
2026-03-09 19:42:26 -0700

    ejercicio 1

--- app/src/main/java/com/example/actividad1_proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/actividad1_proyecto1/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.actividad1_proyecto1.ui.screens.TaskListScreen
 import com.example.actividad1_proyecto1.ui.theme.Actividad1Proyecto1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Actividad1Proyecto1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TaskListScreen()
             }
         }
     }
